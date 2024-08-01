5cc456ee7b4a18c9308196955efec67264251559
yjk0423 <yjk2302126@naver.com>
2024-08-01 19:45:07 +0900

    uploard

---
+++ pyworks/input1.py
@@ -0,0 +1,18 @@
+#입략 함수 - input(문자열)
+# name = jk
+
+#name = input("이름 입력: ")
+#print(f"당신의 이름은 {name} 이군요")
+
+
+number = input("숫자 입력 : ")
+print(type(number))# <- strs
+
+print(int(number) + 1)
+
+num1 = input("1번 수 입력 : ")
+num2 = input("2번 수 입력 : ")
+
+print(int(num1) + int(num2))
+
+print(num1 + num2)
---
+++ pyworks/op2.py
@@ -0,0 +1,20 @@
+#op 복합 대입 연산자
+val = 1
+
+#val = val + 10
+val += 10
+print(val)
+val -= 10
+print(val)
+val *= 10
+print(val)
+val /= 10
+print(val)
+print(type(val))
+
+#청바지 수량
+jean = 0; # 정수 초기값
+#msg = ""; 빈문자열 초기값
+#jean = jean + 1
+jean += 1
+print("청바지 수량: ", jean)
