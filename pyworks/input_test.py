a5361498adaa07c9be6d2a6323e84b7d071dcf2f
yjk0423 <yjk2302126@naver.com>
2024-08-01 20:46:38 +0900

    uploard

--- pyworks/input_test.py
+++ pyworks/input_test.py
@@ -5,10 +5,14 @@ age = int(input("나이를 입력하시오: "))
 print(f"안녕하세요!"+name+f"님 ({age}세)")
 
 #실습2
-name2 = input("이름을 입력하시오: ")
-birth_year = int(input("태어난 년도를 입력하세요.: "))
-year = int(input("올해 년도를 입력하세요.: "))
+try:
+    name2 = input("이름을 입력하시오: ")
+    birth_year = int(input("태어난 년도를 입력하세요.: "))
+    year = int(input("올해 년도를 입력하세요.: "))
 
-succes = year - birth_year
+    succes = year - birth_year
 
-print(f"올해는{year}년,{name2}님의 나이는{succes}세 입니다")
+    print(f"올해는{year}년,{name2}님의 나이는{succes}세 입니다")
+
+except ValueError:
+    print("정수로 입력해주세요.")
