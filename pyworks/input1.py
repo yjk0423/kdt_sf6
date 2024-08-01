1a9994748d246fb900a0db104ae9d889511b05a9
yjk0423 <yjk2302126@naver.com>
2024-08-01 20:38:37 +0900

    uploard

--- pyworks/input1.py
+++ pyworks/input1.py
@@ -5,8 +5,8 @@
 #print(f"당신의 이름은 {name} 이군요")
 
 
-number = input("숫자 입력 : ")
-print(type(number))# <- strs
+number = int(input("숫자 입력 : "))
+print(type(number))
 
 print(int(number) + 1)
 
@@ -16,3 +16,15 @@ num2 = input("2번 수 입력 : ")
 print(int(num1) + int(num2))
 
 print(num1 + num2)
+
+#오류 처리(try ~ except 구문)
+# :(콜론) - 코드 블럭 ({ })
+
+try:
+    num1 = input("1번 수 입력 : ")
+    num2 = input("2번 수 입력 : ")
+
+print(int(num1) + int(num2))
+
+except:
+    print("정수 입력하시오.")
---
+++ pyworks/input_test.py
@@ -0,0 +1,14 @@
+#실습1
+name = input("이름을 입력하시오: ")
+age = int(input("나이를 입력하시오: "))
+
+print(f"안녕하세요!"+name+f"님 ({age}세)")
+
+#실습2
+name2 = input("이름을 입력하시오: ")
+birth_year = int(input("태어난 년도를 입력하세요.: "))
+year = int(input("올해 년도를 입력하세요.: "))
+
+succes = year - birth_year
+
+print(f"올해는{year}년,{name2}님의 나이는{succes}세 입니다")
